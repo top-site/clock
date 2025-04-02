59c85fb3c49594370c646aa8e4e405d0e282f032
top-site <drsalehm2002@hotmail.com>
2025-04-02 06:06:26 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,8 @@ self.addEventListener("install", (event) => {
         "/index.html",
         "/styles.css",
         "/script.js",
-        "/icons/icon-192x192.png",
-        "/icons/icon-512x512.png"
+        "/icon-192x192.png",
+        "/icon-512x512.png"
       ]);
     })
   );
